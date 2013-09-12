7c7efb4acb9eba50ad8f9458e50f149c587ade78
innocentboy <innocentboy1386@gmail.com>
2013-09-12 17:31:20 +0530

    final code of IP is commited..

--- src/specical.cpp
+++ src/specical.cpp
@@ -453,6 +453,341 @@ namespace Temp
 
             return n;
         }
+        
+        
+/**
+ * //IP address are simulated as  a.b.c.d as a string, a,b,c,d is used in the programm according to this conventon.
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <malloc/malloc.h>
+#include <assert.h>
+#include <stdlib.h>
+#define N 255256
+#define INPUTSTREAM1 "test_demo.txt"
+#define INPUTSTREAM2 "test1_demo.txt"
+#define OUTSTREAM1 "optput1.txt"
+#define OUTSTREAM2 "optput2.txt"
+#define INTERSECTIPSTREAM "intersectIP.txt"
+
+
+typedef struct stud {
+    int val;
+    char status;
+    char a[4];
+    char d[4];
+    struct stud *next;
+}Node;
+
+typedef struct hashstruct
+{
+    char b[4];
+    char c[4];
+    Node *nPtr;
+}Hash;
+
+//initialises the map for providing hashing..
+void hashInit(Hash *mp)
+{
+    int i;
+    for(i=0;i<N;i++)
+        mp[i].nPtr=NULL;
+}
+
+
+//insert the Node in the increasing sorted array..
+void insert(Hash *mp,Node *dest,int idx)
+{
+    Node *src=NULL,*p=NULL;
+    src=mp[idx].nPtr;
+    
+    if(!src)
+    {
+        mp[idx].nPtr=dest;
+    }
+    
+    while (src&&src->val<=dest->val) {
+        if(src->val==dest->val){
+            free(dest);
+            return;
+        }
+        p=src;
+        src=src->next;
+        
+    }
+    if(!p)
+    {
+        dest->next=src;
+        mp[idx].nPtr=dest;
+    }
+    else if(!src)
+    {
+        p->next=dest;
+    }
+    else
+    {
+        dest->next=p->next;
+        p->next=dest;
+    }
+    
+}
+
+
+//get the Integer value from the string
+int strToInt(char *a,char *b)
+{
+    int i=0,idx=0,flag=0,j=0,mHash=10;    
+    while(a[i]!='\0')
+    {
+        while (a[i]=='0'&&flag==0) {
+            i++;
+        }
+        if(a[i]=='\0')
+        {
+            break;
+        }
+        flag=1;
+        idx = idx*mHash+(a[i]-'0');
+        i++;
+    }
+    flag=0;j=0;
+    i=0;
+    
+    j=(int)strlen(b);
+    int k=0;
+    while (k<3-j) {
+        idx*=10;
+        k++;
+    }
+    
+    while(b[i]!='\0')
+    {
+        idx = idx*mHash+(b[i]-'0');
+        i++;
+    }    
+    return idx;
+}
+
+
+//copies the string value to different value
+void strCopy(char *dest,char *src,int s,size_t e)
+{
+    int j=0;
+    while (s<=e) {
+        dest[j]=src[s];
+        j++;s++;
+    }
+    dest[j]='\0';
+}
+
+//Generates the different possible cominations.
+void generateComb(char *a,char *d,char *b,char *c,Hash *mp)
+{
+    int i,j,alen,dlen;
+    int idx=0;
+    alen=(int)strlen(a);
+    dlen=(int)strlen(d);
+    if(atoi(b)>255||atoi(c)>255)
+    {
+        return;
+    }
+    idx=strToInt(b, c);
+    if(mp[idx].nPtr==NULL){
+            strCopy(mp[idx].b,b,0,strlen(b)-1);
+            strCopy(mp[idx].c,c,0,strlen(c)-1);
+    }
+    
+    for(i=alen-1;i>=0;i--)
+    {
+        for(j=0;j<dlen;j++)
+        {
+            Node *ptr=(Node *)malloc(sizeof(Node));
+            strCopy(ptr->a, a, i, alen);
+            strCopy(ptr->d, d, 0, j);
+            ptr->val=strToInt(ptr->a, ptr->d);
+            ptr->next=NULL;
+            if(atoi(ptr->a)>255||atoi(ptr->d)>255||atoi(ptr->a)==0||atoi(ptr->d)==0)
+            {
+                free(ptr);
+            }
+            else{
+                insert(mp,ptr,idx);
+            }
+        }
+    }
+}
+
+//common parser method
+Hash* parseFileDataStream(const char *fname)
+{
+    Hash *mp=(Hash *)malloc(sizeof(Hash)*N);
+    hashInit(mp);
+    int k=0,tempIdx=0,nOctact=0,len=0;
+    char temp[4],temp1[4],charcter[4];
+    char a[4],b[4],c[4],d[4];
+    char ch,prvChar='\0';
+    FILE *fptr=fopen(fname, "r");
+    if(!fptr){
+        perror("falied to open the file..");
+        return NULL;
+    }
+    a[0]=b[0]=c[0]=d[0]='\0';
+    while (( fscanf(fptr,"%c",&ch) ) != EOF) {
+        fputc(ch, fptr);
+        if(ch!='.'){
+            len++;
+            temp[(len-1)%3]=ch;
+            
+            if(len<4)
+                temp1[len-1]=ch;
+            
+            if(len>3)
+            {
+                tempIdx=(len-1)%3;
+            }
+        }
+        else if(prvChar=='.'&&ch=='.')
+        {
+            len=0;
+            nOctact=0;
+        }
+        
+        else
+        {
+            nOctact++;
+            temp1[3]='\0';
+            if(len<=3)
+                tempIdx=0;
+            else
+                tempIdx=(tempIdx+1)%3;
+            
+            int i=tempIdx,j;
+            j=0;
+            do{
+
+                charcter[j]=temp[i];
+                j++;
+                if(len>3)
+                    i=(i+1)%3;
+                else
+                    i=(i+1)%len;
+                //  printf("\ni:%d  idx:%d  len:%d\n",i,tempIdx,len);
+            }while(i!=tempIdx);
+            
+            if(len>3)
+                charcter[3]='\0';
+            else
+                charcter[len]='\0';
+            // printf("\n****** %d ******\n",nOctact);
+            switch(nOctact)
+            {
+                case 1:
+                    strCopy(a,charcter,0,strlen(charcter)-1);
+                    break;
+                case 2:
+                    strCopy(b,charcter,0,strlen(charcter)-1);
+                    break;
+                case 3:
+                    strCopy(c,charcter,0,strlen(charcter)-1);
+                    break;
+                case 4:
+                    if(len>3)
+                        k=3;
+                    else
+                        k=len;
+                    // printf("\nprevdot:%d len:%d\n",prevDot,prevDot+len);
+                    strCopy(d,temp1,0,k-1);
+                    // printf("\ns1**** a:%s b:%s c:%s d:%s\n",a,b,c,d);
+                    generateComb(a,d,b,c,mp);
+            }
+            if(len>3)
+            {
+                nOctact=1;
+                strCopy(a,charcter,0,strlen(charcter)-1);
+            }
+            else if(nOctact==4)
+            {
+                nOctact=3;
+                strCopy(a,b,0,strlen(b)-1);
+                strCopy(b,c,0,strlen(c)-1);
+                strCopy(c,d,0,strlen(d)-1);
+            }
+            len=0;
+        }
+        prvChar=ch;
+    }
+    
+    if(nOctact==3)
+    {
+        if(len>3)
+            k=3;
+        else
+            k=len;
+        strCopy(d,temp1,0,k-1);
+        // printf("\ns1**** a:%s b:%s c:%s d:%s\n",a,b,c,d);
+        generateComb(a,d,b,c,mp);
+    }
+    fclose(fptr);
+    return mp;
+}
+
+
+//Function that gets the intersected IP addresses.
+FILE* getIntersection(const Hash *mp1,const Hash *mp2)
+{
+    FILE *intersectIPstream;
+    intersectIPstream=fopen(INTERSECTIPSTREAM, "w");
+    if(!intersectIPstream)
+    {
+        perror("Some problem in Openning the file");
+        return NULL;
+    }
+    int i=0;
+    Node *ptr1,*ptr2;
+    for(i=0;i<N;i++)
+    {
+        ptr1=mp1[i].nPtr;
+        ptr2=mp2[i].nPtr;
+        while (ptr1!=NULL&&ptr2!=NULL) {
+            if(ptr1->val==ptr2->val)
+            {
+                if(intersectIPstream)
+                fprintf(intersectIPstream,"\n%s.%s.%s.%s",ptr1->a,mp1[i].b,mp1[i].c,ptr1->d);
+                printf("\n%s.%s.%s.%s",ptr1->a,mp1[i].b,mp1[i].c,ptr1->d);
+                ptr1=ptr1->next;
+                ptr2=ptr2->next;
+            }
+            else if(ptr1->val<ptr2->val)
+                ptr1=ptr1->next;
+            else
+                ptr2=ptr2->next;
+        }
+    }
+    return intersectIPstream;
+}
+
+//this is the main function
+int main(int argc,char **argv)
+{
+    
+    FILE *intersectIPstream;
+    Hash *mp1,*mp2;
+    intersectIPstream=fopen(INTERSECTIPSTREAM, "w");
+    mp1=parseFileDataStream(INPUTSTREAM1);
+    mp2=parseFileDataStream(INPUTSTREAM2);
+    if(!mp1||!mp2)
+    {
+        perror("either of the file is not read properly");
+        return -1;
+    }
+    intersectIPstream=getIntersection(mp1,mp2);
+    
+    return  0;
+}
+
+
+ * /
 
         public static long Sqr(long x)
         {
