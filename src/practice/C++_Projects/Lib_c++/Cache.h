1c1b02e862b59bb556a44d204940bac3fe9015c5
innocentboy <innocentboy1386@gmail.com>
2013-09-18 18:02:27 -0700

    lib filder is commited

---
+++ src/practice/C++_Projects/Lib_c++/Cache.h
@@ -0,0 +1,15 @@
+#ifndef __LIB_HASH_H
+#define __LIB_HASH_H
+
+#include <iostream>
+#include <stddef.h>
+#include <stdint.h>
+
+using namespace std;
+
+
+
+
+
+
+#endif
---
+++ src/practice/C++_Projects/Lib_c++/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "list_test.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    /**
+    Testing function for the generic list.
+    */
+    //test_list_demo();
+
+    /**
+    Testing function for the generic hash.
+    */
+
+    return 0;
+}
