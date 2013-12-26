dfb6be5188bc57c5d60fcc926a1bfbe83c0c3f05
innocentboy <innocentboy1386@gmail.com>
2013-12-26 20:29:41 +0530

    maxsum.cpp file is commited.

---
+++ src/codee/codchef/MaxSum.cpp
@@ -0,0 +1,28 @@
+/**
+LINK:::http://www.codechef.com/problems/KSUBSUM
+*/
+/**
+INPUT:
+3
+3 1 2 3
+10 20 30
+3 3 4 6
+10 20 30
+4 2 6 10
+20 -15 10 -15
+
+OUTPUT:
+60 50 30
+30 30 10
+15 -5 -20
+*/
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define N 10000
+
+void driver()
+{
+	printf("\ndriver is running..\n");
+}
