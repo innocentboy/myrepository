b9e585c10903a8871f07661959d4e118ed2331d6
innocentboy <innocentboy1386@gmail.com>
2013-12-26 19:58:03 +0530

    tree and subtree file commited

---
+++ src/codee/codchef/Trees_And_Subtrees.cpp
@@ -0,0 +1,35 @@
+/**
+EASY
+LINK:http://www.codechef.com/problems/TRSUBTR
+*/
+/**
+INPUT:
+5
+1
+2
+1
+2
+1
+1 2 M
+1 3 E
+2 4 E
+3 5 M
+2
+2
+1
+2
+1 2 M
+2
+1
+1
+1 2 M
+
+OUTPUT:
+NO
+YES
+*/
+
+void driver()
+{
+	printf("\nDriver is running..\n");
+}
