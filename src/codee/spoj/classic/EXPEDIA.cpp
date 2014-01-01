01ab8555882a44d2d4deffa6ff1b14b7c59edc02
innocentboy <innocentboy1386@gmail.com>
2014-01-01 15:30:55 +0530

    expedia commited

---
+++ src/codee/spoj/classic/EXPEDIA.cpp
@@ -0,0 +1,25 @@
+/**
+http://www.spoj.com/problems/EXPEDI/
+*/
+/**
+INPUT:
+
+OUTPUT:
+
+*/
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <map>
+using namespace  std;
+#define N 100005
+
+void driver()
+{
+	printf("\nExpedia driver is running..\n");
+
+}
