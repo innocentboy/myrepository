a5d9877dba0a24340d15bd050da71658520cd41c
innocentboy <innocentboy1386@gmail.com>
2014-01-02 19:18:49 +0530

    Create BST.cpp

---
+++ src/codee/spoj/classic/BST.cpp
@@ -0,0 +1,40 @@
+/**
+http://www.spoj.com/problems/BST/
+*/
+/**
+INPUT:
+8
+3
+5
+1
+6
+8
+7
+2
+4
+
+OUTPUT:
+0
+1
+2
+4
+7
+11
+13
+15
+*/
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <map>
+using namespace  std;
+#define N 100005
+
+void driver()
+{
+	printf("\ndriver of BST is runniong\n");
+}
