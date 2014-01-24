6c0d6e031f038c33e151b0248ce4e47679587bc8
innocentboy <innocentboy1386@gmail.com>
2014-01-24 20:26:27 +0530

    Create NEXTNUM.cpp

---
+++ src/codee/codchef/NEXTNUM.cpp
@@ -0,0 +1,34 @@
+/**
+http://www.codechef.com/problems/NEXTNUM
+*/
+
+/**
+INPUT:
+2
+276
+762
+
+OUTPUT:
+2
+6
+
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <map>
+#include <cstring>
+using namespace  std;
+#define N 6666
+#define INF 1000000000
+#define ull unsigned long long 
+
+void driver()
+{
+	printf("\ndriver of NEXTNUM is running..\n");
+}	
