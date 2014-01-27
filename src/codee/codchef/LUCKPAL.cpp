3aa94451f7624597139f1917fde330c2f9333c71
innocentboy <innocentboy1386@gmail.com>
2014-01-27 19:10:19 +0530

    Create LUCKPAL.cpp

---
+++ src/codee/codchef/LUCKPAL.cpp
@@ -0,0 +1,39 @@
+/**
+http://www.codechef.com/problems/LUCKPAL
+
+*/
+/**
+Input
+
+3
+laubcdkey
+luckycodechef
+aaaaaaaa
+
+Output
+
+luckykcul 8
+luckycocykcul 6
+unlucky
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <map>
+#include <cstring>
+using namespace  std;
+#define N 1005
+#define M 1000000007
+
+//int main()
+void driver()
+{
+	printf("\ndriver of LUCKPAL is running\n");
+
+	//return 0;
+}
