f68854e96aae46559263098ef298fbacf4ce4707
innocentboy <innocentboy1386@gmail.com>
2014-02-03 21:48:06 -0800

    query c ommited

---
+++ src/codee/codchef/QUERY.cpp
@@ -0,0 +1,30 @@
+/**
+http://discuss.codechef.com/questions/6548/query-editorial
+*/
+
+/**
+
+*/
+
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <stack>
+#include <map>
+#include <cstring>
+#include <queue>
+using namespace  std;
+#define N 100
+#define M 100
+#define INF 1000000000
+#define ull long long
+
+int main()
+{
+
+
+    return 0;
+}
